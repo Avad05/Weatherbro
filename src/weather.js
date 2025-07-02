26f5e13c3ea7a2efc1196b1c55b1c041a9d0bad3
Avadhut <avadhutkaskar@gmail.com>
2025-07-02 21:45:22 +0530

    Next 15 days data Remaining

--- src/weather.js
+++ src/weather.js
@@ -18,7 +18,7 @@ async function getWeather(name){
     }
 
     const api = 'U5EW32CQS23T7AYT84HR5TV39';
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${name}/next5days?unitGroup=us&key=${api}&elements=tempmax,tempmin,feelslike,conditions,icon,dew,humidity&iconSet=icons1`
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${name}/next15days?unitGroup=us&key=${api}&elements=tempmax,tempmin,feelslike,conditions,icon,dew,humidity,windspeed,cloudcover&iconSet=icons1`
 
     
     let user;
@@ -27,8 +27,15 @@ async function getWeather(name){
     
        try{
          user = await fetch(url, {mode: 'cors'})
-         if(user.status === 429){
+         if(!user.ok){
+            throw new Error (`HTTP: Error ${user.status}`);
+         }
+        /* if(user.status === 429){
             throw new Error ("rate limit exceeded, try later");
+            }
+            if(user.status === 400){
+                alert("No such city found");
+                throw new Error ("No Such City found");               
             }
          if(user.status === 404){
             throw new Error ("Match not found");
@@ -41,15 +48,22 @@ async function getWeather(name){
          }
          if(!user.status === 200){
              throw new Error ("Bro What error is this");
-         }
+         }*/
          Data = await user.json();
          console.log(Data);
     }catch(error){
         console.log("DATA not found:" + error);
     }
+   
+let days;    
+if(Data && Data.days){
+days = Data.days;
+}
+else{
+    console.log("Something wrong");
+}
 
-    const days = Data.days;
-
+/* const days = Data.days;*/
     resolvedAddress = Data.resolvedAddress;
     document.getElementById('place').textContent = resolvedAddress;
     days.forEach((days, index) => {
@@ -58,10 +72,14 @@ async function getWeather(name){
             const  iconId = `${days.icon}`;
             console.log(iconId);
              img.src = `/icons/${iconId}.png`;
-            container.querySelector(".feelslike").textContent = `${days.feelslike}`;
+            container.querySelector(".feelslike").textContent = `${days.feelslike}°F`;
             container.querySelector(".condition").textContent = `${days.conditions}`;
-            container.querySelector(".minValue").textContent = `${days.tempmin}`;
-            container.querySelector(".maxValue").textContent = `${days.tempmax}`;
+            container.querySelector(".minValue").textContent = `${days.tempmin}°F`;
+            container.querySelector(".maxValue").textContent = `${days.tempmax}°F`;
+            container.querySelector(".visibility").textContent = `${days.dew}%`;
+            container.querySelector(".wind").textContent = `${days.windspeed}m/s`;
+            container.querySelector(".humidity").textContent = `${days.humidity}%`;
+            container.querySelector(".cloudyness").textContent = `${days.cloudcover}%`;
         }
         
     });
