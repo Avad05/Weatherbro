43e5795db88d04c47ed2eec14914da045e26bb7f
Avadhut <avadhutkaskar@gmail.com>
2025-07-03 21:31:11 +0530

    conversion done

--- src/weather.js
+++ src/weather.js
@@ -9,17 +9,18 @@ const container1 = document.querySelector('.current_one');
 const container2 = document.querySelector('.current_two');
 const img = document.querySelector(".logo");
 const slider = document.querySelector(".slider");
+const degree = document.getElementById('degree');
+const api = 'U5EW32CQS23T7AYT84HR5TV39';
 
 
-
-async function getWeather(name){
+async function getWeather(name, degrees, unit){
     if(!name.trim() || !name){
         alert("Enter a city name");
         return;
     }
 
-    const api = 'U5EW32CQS23T7AYT84HR5TV39';
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${name}/next8days?unitGroup=us&key=${api}&elements=tempmax,tempmin,feelslike,conditions,icon,dew,humidity,windspeed,cloudcover,datetime&iconSet=icons1`
+   
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${name}/next8days?unitGroup=${degrees}&key=${api}&elements=tempmax,tempmin,feelslike,conditions,icon,dew,humidity,windspeed,cloudcover,datetime&iconSet=icons1`
 
     
     let user;
@@ -31,25 +32,7 @@ async function getWeather(name){
          if(!user.ok){
             throw new Error (`HTTP: Error ${user.status}`);
          }
-        /* if(user.status === 429){
-            throw new Error ("rate limit exceeded, try later");
-            }
-            if(user.status === 400){
-                alert("No such city found");
-                throw new Error ("No Such City found");               
-            }
-         if(user.status === 404){
-            throw new Error ("Match not found");
-         }  
-         if(user.status === 500){
-            throw new Error ("Internal Error");
-         } 
-         if(user.status === 401){
-            throw new Error ("Problem with API key");
-         }
-         if(!user.status === 200){
-             throw new Error ("Bro What error is this");
-         }*/
+       
          Data = await user.json();
          console.log(Data);
     }catch(error){
@@ -76,10 +59,10 @@ else{
             const  iconId = `${days.icon}`;
             console.log(iconId);
              img.src = `/icons/${iconId}.png`;
-            container.querySelector(".feelslike").textContent = `${days.feelslike}°F`;
+            container.querySelector(".feelslike").textContent = `${days.feelslike}${unit}`;
             container.querySelector(".condition").textContent = `${days.conditions}`;
-            container.querySelector(".minValue").textContent = `${days.tempmin}°F`;
-            container.querySelector(".maxValue").textContent = `${days.tempmax}°F`;
+            container.querySelector(".minValue").textContent = `${days.tempmin}${unit}`;
+            container.querySelector(".maxValue").textContent = `${days.tempmax}${unit}`;
             container.querySelector(".visibility").textContent = `${days.dew}%`;
             container.querySelector(".wind").textContent = `${days.windspeed}m/s`;
             container.querySelector(".humidity").textContent = `${days.humidity}%`;
@@ -90,7 +73,7 @@ else{
            container.querySelector(".date").textContent = `${days.datetime}`;
            const  iconId = `${days.icon}`;
              container.querySelector(".icon").src = `/icons/${iconId}.png`;
-           container.querySelector(".feels").textContent = `${days.feelslike}°F`;
+           container.querySelector(".feels").textContent = `${days.feelslike}${unit}`;
            }
         }
 
@@ -105,8 +88,28 @@ else{
 
 submit.addEventListener('click', ()=>{
     const cityName = city.value;
-    getWeather(cityName);
+    let country = "uk";
+    let units = "°C";
+    getWeather(cityName, country, units);
 })
+
+degree.addEventListener('click', () => {
+    if(degree.textContent === "°F"){
+        degree.textContent = "°C";
+        const cityName = city.value;       
+        let country = "uk";
+        let units = "°C";
+        getWeather(cityName, country, units);
+
+    }
+    else{
+        degree.textContent = "°F";
+        const cityName = city.value;
+        let country = "us";
+        let units = "°F"
+        getWeather(cityName, country, units);
+    }
+});
 
 
 
